c65a1fae67aafcee60b037d0734493418a561156
RichaWadekar <105279852+RichaWadekar@users.noreply.github.com>
2022-06-29 20:19:08 +0530

    Add files via upload

---
+++ personal.js
@@ -0,0 +1,9 @@
+// typing annimation //
+
+var typed = new Typed(".typing",{
+
+    strings:["","Full Stack Java Developer","Web Developer","Graphics designer",""],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
